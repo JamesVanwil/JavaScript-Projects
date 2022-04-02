2527bd4f493e94b5b602bb9f8549d0b80eb444a4
JamesVanwil <100517278+JamesVanwil@users.noreply.github.com>
2022-04-02 13:11:21 +0200

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var A = "Hello." //Variable A
+window.alert(A) //Alert with variable A
+var B = "Hello again" // Variable B
+document.write(B) //Writes variable B
+document.write(6+5) //Adds 6+5
+
+var breed = 'German shepard' //Var breed
+var weight = '20kg' //Var weight
+document.write("My dog is a " + breed + " and he weighs " + weight + " that is heavy"); //Writes text + variables
+
+
+var hi = "WOW"; // Var hi
+var hi = hi.fontcolor("blue"); //Changes color to blue
+document.write(hi); //Writes the hi variable
+
+
+
+
+
