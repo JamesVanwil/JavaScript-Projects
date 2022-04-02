91d55c07c6d4385df09cbc5c646c97aa3dfdc0c4
JamesVanwil <100517278+JamesVanwil@users.noreply.github.com>
2022-04-02 13:40:55 +0200

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //Calling my function
+    var sentence = "I am learning"; //Sentence to display
+    sentence += " JavaScript"; //Sentence to display 
+    document.getElementById("Text").innerHTML = sentence; //Linking my HTML Id to js so that when button clicked text changes
+}
