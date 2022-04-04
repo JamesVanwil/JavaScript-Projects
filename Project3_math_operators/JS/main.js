3e64cc9d49ea4d671900dd4af90b4befeb8c0c9a
JamesVanwil <100517278+JamesVanwil@users.noreply.github.com>
2022-04-04 12:41:58 +0200

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,32 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition; //This would give us the result of 2 + 2
+}
+
+function subtraction_Function() {
+    var Subtraction = 10 - 5;
+    document.getElementById("Math").innerHTML = "10 - 5 = " + Subtraction; // This would give us the result of 10 - 5
+}
+
+window.alert(Math.random() * 100); //Selects a random number between 0 and 100
+
+var X = 10;
+X++;
+document.write(X); //This would increment the number 10 by 1 
+
+
+var X = 11;
+X--;
+document.write(X); //This would decrement the number 11 by 1
+
+function modulus_Operator() {
+    var simple_Maths = 25 % 8;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 8 you have the remainder of: " + simple_Maths; //This would give us the remainder of 25/8
+}
+
+function more_Math() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math; //This would give us the result of 6 x 8 
+}
+
+
